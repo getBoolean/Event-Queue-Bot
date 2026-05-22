340dd9c10d59e8169770275248a1e5b6b752ff6f
BooleanDev <19920697+getBoolean@users.noreply.github.com>
2026-05-22 04:31:43 -0500

    enable migrations on db connect

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,24 @@
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 
 import * as schema from "./schema.ts";
 
 export const DB_FILEPATH = "data/main.sqlite";
 export const DB_BACKUP_DIRECTORY = "data/backups";
-export let db = drizzle(Database(DB_FILEPATH).defaultSafeIntegers(), { schema });
+export const MIGRATIONS_FOLDER = "data/migrations";
+
+function connect() {
+	const conn = drizzle(Database(DB_FILEPATH).defaultSafeIntegers(), { schema });
+	migrate(conn, { migrationsFolder: MIGRATIONS_FOLDER });
+	return conn;
+}
+
+export let db = connect();
 
 export namespace Db {
 	export function reload() {
-		db = drizzle(Database(DB_FILEPATH).defaultSafeIntegers(), { schema });
+		db = connect();
 	}
 
 	export function printLoadMessage() {
