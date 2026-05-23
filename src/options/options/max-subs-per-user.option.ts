55eec6b16f45a1107c9cf317ebabe26dad7929a7
BooleanDev <19920697+getBoolean@users.noreply.github.com>
2026-05-22 20:42:03 -0500

    remove default value

--- src/options/options/max-subs-per-user.option.ts
+++ src/options/options/max-subs-per-user.option.ts
@@ -1,9 +1,7 @@
-import { EVENT_TABLE } from "../../db/schema.ts";
 import { IntegerOption } from "../base-option.ts";
 
 export class MaxSubsPerUserOption extends IntegerOption {
 	static readonly ID = "max_subs_per_user";
 	id = MaxSubsPerUserOption.ID;
 	minValue = 0;
-	defaultValue = EVENT_TABLE.maxSubsPerUser.default;
 }
